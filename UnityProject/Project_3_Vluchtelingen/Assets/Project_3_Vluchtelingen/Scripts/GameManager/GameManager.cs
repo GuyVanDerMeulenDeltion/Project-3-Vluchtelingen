49d75fe8ede8b6fc90aa5098bea8919e452a8c1c
GuyVanDerMeulenDeltion <guy_meulen@live.nl>
2017-02-06 22:46:53 +0100

    Updated AI, cleaned out inspector, editted logbook formats
    no lenny today :cactus:

---
+++ UnityProject/Project_3_Vluchtelingen/Assets/Project_3_Vluchtelingen/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    [Header("AI Settings:")]
+
+    [Tooltip("This defines the speed the NPC rotates at.")]
+    [Range(0, 10f)]
+    public float npcRotationSpeed = 5;
+
+    [Tooltip("This is the movement speed of this NPC.")]
+    [Range(0, 50f)]
+    public float npcWalkingSpeed = 5;
+
+    [Tooltip("This is the max amount of seconds the NPC will wait after collision with the Player.")]
+    [Range(0, 5f)]
+    public float npcWalkingTimerMaximum = 2;
+
+    [Tooltip("The NPC will follow this specific GameObject.")]
+    public GameObject target;
+
+    [Header("Player Settings:")]
+
+    [Tooltip("This is the Position of the Player.")]
+    public Transform playerPositionAndRotation;
+
+}
