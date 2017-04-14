c99332e0e634193f24846281f2e9178ca05d7ac6
ThomasVlekkeDeltion <97042453@st.deltion.nl>
2017-04-14 19:53:24 +0200

    Char.Controller + Sleutels

---
+++ UnityProject/Scene - Thomas/Assets/Classes/Char.Controller/Movement.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public Vector3 camRotatie;
+    public Vector3 rotatieS;
+    public GameObject cam;
+    public float speed;
+    public float adjustmentsspeed;
+    public float minaxis;
+    public float maxaxis;
+    public Vector3 links;
+    public float hor;
+    public float ver;
+    public float accel;
+
+    void Start()
+    {
+
+    }
+
+
+    void FixedUpdate()
+    {
+       
+        hor = Input.GetAxis("Horizontal");
+        ver = Input.GetAxis("Vertical");
+
+      
+        links.x = hor;
+        links.z = ver;
+
+       
+        transform.Translate(links * Time.deltaTime * accel);
+
+
+    }
+
+    void Update()
+    {
+        
+        if (Input.GetButtonDown("o"))
+        {
+            decreasesen();
+        }
+
+     
+        if (Input.GetButtonDown("p"))
+        {
+            increasesen();
+        }
+
+      
+        if (speed < 1)
+        {
+            speed = 1;
+        }
+
+       
+        if (speed < minaxis)
+        {
+            speed = minaxis;
+        }
+
+     
+        if (speed > maxaxis)
+        {
+            speed = maxaxis;
+        }
+
+  
+        rotatieS = rotatieS * Time.deltaTime;
+        camRotatie = camRotatie * Time.deltaTime;
+
+      
+        rotatieS.y = Input.GetAxis("Mouse X") * speed;
+        camRotatie.x = Input.GetAxis("Mouse Y") * -speed;
+
+        transform.Rotate(rotatieS);
+        cam.transform.Rotate(camRotatie);
+
+
+
+    }
+
+    public void increasesen()
+    {
+        speed = speed + adjustmentsspeed;
+    }
+
+    public void decreasesen()
+    {
+        speed = speed - adjustmentsspeed;
+    }
+}
